4b33e803094a4aa87ea91f3d9b6df9b7591ef614
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-01-06 12:46:00 -0500

    feature: user user crud complete

--- advance-server/src/Api/Controllers/UserController.js
+++ advance-server/src/Api/Controllers/UserController.js
@@ -1,7 +1,3 @@
-/*
-res.status(200).json({ message: "✅ yeeeeessss.", info:user })
-res.status(400).json({ message: "❌ nooooooo.", error: error.message })
- */
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
 
@@ -20,6 +16,20 @@ export const getUser = async (req, res) => {
     res
       .status(500)
       .json({ message: "❌ user not found:", error: error.message });
+  }
+};
+
+/* Get all users */
+export const getAllUsers = async (req, res) => {
+  try {
+    let users = await UserModel.find();
+    users = users.map((user)=>{
+      const {password, ...otherDetails} = user._doc
+      return otherDetails
+    })
+    res.status(200).json({message: "✅ multiple users found",users:users});
+  } catch (error) {
+    res.status(500).json({message: "❌ multipe users not found:", error: error.message});
   }
 };
 
@@ -65,5 +75,60 @@ export const deleteUser = async (req, res) => {
     }
   } else {
     res.status(403).json({ message: "❌ delete access denied."})
+  }
+};
+
+/* Follow a User */
+export const followUser = async (req, res) => {
+  const id = req.params.id;
+  const { _id } = req.body;
+  if (_id == id) {
+    res.status(403).json({message:"❌ Action Forbidden"});
+  } else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(_id);
+
+      if (!followUser.followers.includes(_id)) {
+        await followUser.updateOne({ $push: { followers: _id } });
+        await followingUser.updateOne({ $push: { following: id } });
+        res.status(200).json({messsage: "✅ User followed!",followUser:followUser,followingUser:followingUser});
+      } else {
+        res.status(403).json({messsage: "❌ you are already following this id"});
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ message: "❌ follow user catch.", error: error.message })
+    }
+  }
+};
+
+/* Unfollow a User */
+export const unfollowUser = async (req, res) => {
+  const id = req.params.id;
+  const { _id } = req.body;
+
+  if(_id === id)
+  {
+    res.status(403).json("Action Forbidden")
+  }
+  else{
+    try {
+      const unFollowUser = await UserModel.findById(id)
+      const unFollowingUser = await UserModel.findById(_id)
+
+
+      if (unFollowUser.followers.includes(_id))
+      {
+        await unFollowUser.updateOne({$pull : {followers: _id}})
+        await unFollowingUser.updateOne({$pull : {following: id}})
+        res.status(200).json({message:"✅ Unfollowed Successfully!",followUser:followUser,followingUser:followingUser})
+      }
+      else{
+        res.status(403).json({message:"❌ You are not following this User"})
+      }
+    } catch (error) {
+      res.status(500).json({ message: "❌ could not unfollow catch:", error: error.message })
+    }
   }
 };
--- advance-server/src/Api/Routes/UserRoute.js
+++ advance-server/src/Api/Routes/UserRoute.js
@@ -1,10 +1,12 @@
 import express  from "express";
-import { deleteUser, getUser, updateUser } from "../Controllers/UserController.js";
+import { deleteUser, followUser, getAllUsers, getUser, unfollowUser, updateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
+router.get('/',getAllUsers)
 router.get("/:id", getUser)
 router.put("/:id", updateUser)
 router.delete("/:id", deleteUser)
-
+router.put("/:id/follow", followUser)
+router.put("/:id/unfollow", unfollowUser)
 export default router;
