0fa384e150b119c162ddf535a5d22eda3b56e3fb
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-01-07 15:37:47 -0500

    feature: create getTimelinePosts controller

--- advance-server/src/Api/Controllers/PostController.js
+++ advance-server/src/Api/Controllers/PostController.js
@@ -1,93 +1,155 @@
 import mongoose from "mongoose";
 import PostModel from "../Models/postModel.js";
+import UserModel from "../Models/userModel.js";
 
 /* create post */
-export const createPost = async (req, res)=> {
+export const createPost = async (req, res) => {
   const newPost = new PostModel(req.body);
   try {
-
-    await newPost.save()
-    res.status(201).json({message:"✅ post created!", post: newPost})
+    await newPost.save();
+    res.status(201).json({ message: "✅ post created!", post: newPost });
   } catch (error) {
-    res.status(500).json({message:"❌ could not create post:" , error: error.message})
+    res
+      .status(500)
+      .json({ message: "❌ could not create post:", error: error.message });
   }
 };
 
 /* get a single post */
-export const  getPost = async(req,res) =>{
+export const getPost = async (req, res) => {
   try {
-    const postForQueryId = req.params.id
-    const postQuery = await PostModel.findById(postForQueryId)
+    const postForQueryId = req.params.id;
+    const postQuery = await PostModel.findById(postForQueryId);
 
-    res.status(200).json({message:"✅ post found:", post:postQuery})
+    res.status(200).json({ message: "✅ post found:", post: postQuery });
   } catch (error) {
-    res.status(500).json({message:"❌ post could not be found", error:error.message})
+    res
+      .status(500)
+      .json({ message: "❌ post could not be found", error: error.message });
   }
-}
+};
 
 /* get multiple posts */
-export const getAllPosts= async (req, res) => {
+export const getAllPosts = async (req, res) => {
   try {
     const posts = await PostModel.find();
-    res.status(200).json({message: "✅ multiple posts found.",posts:posts});
+    res.status(200).json({ message: "✅ multiple posts found.", posts: posts });
   } catch (error) {
-    res.status(500).json({message: "❌ multipe posts not found:", error: error.message});
+    res
+      .status(500)
+      .json({ message: "❌ multipe posts not found:", error: error.message });
   }
 };
 
 /* update post */
 export const updatePost = async (req, res) => {
   try {
-  const postId = req.params.id;
-  const { userId } = req.body;
+    const postId = req.params.id;
+    const { userId } = req.body;
 
     const post = await PostModel.findById(postId);
     if (post.userId === userId) {
-      await post.updateOne({ $set: req.body }, {new: true});
+      await post.updateOne({ $set: req.body }, { new: true });
       const updatedPost = await PostModel.findById(postId);
-      res.status(200).json({message:"✅ post updated:", post:updatedPost});
+      res.status(200).json({ message: "✅ post updated:", post: updatedPost });
     } else {
-      res.status(403).json({message:"❌ Authentication failed"});
+      res.status(403).json({ message: "❌ Authentication failed" });
     }
   } catch (error) {
-    res.status(500).json({message:"❌ post could not be updated:", error:error.message})
+    res
+      .status(500)
+      .json({ message: "❌ post could not be updated:", error: error.message });
   }
 };
 
 /* delete post */
 export const deletePost = async (req, res) => {
   try {
-  const id = req.params.id;
-  const { userId } = req.body;
+    const id = req.params.id;
+    const { userId } = req.body;
 
     const post = await PostModel.findById(id);
     if (post.userId === userId) {
       const deletedPost = await post.deleteOne();
-      res.status(200).json({message:"✅Post deleted.",deletedPost:deletedPost});
+      res
+        .status(200)
+        .json({ message: "✅Post deleted.", deletedPost: deletedPost });
     } else {
-      res.status(403).json({message:"❌Action forbidden"});
+      res.status(403).json({ message: "❌Action forbidden" });
     }
   } catch (error) {
-    res.status(500).json({message:"❌ post could not be deleted",error:error.message});
+    res
+      .status(500)
+      .json({ message: "❌ post could not be deleted", error: error.message });
   }
 };
 
 /* like & dislike post */
 export const likePost = async (req, res) => {
   try {
-  const id = req.params.id;
-  const { userId } = req.body;
+    const id = req.params.id;
+    const { userId } = req.body;
     const post = await PostModel.findById(id);
     if (post.likes.includes(userId)) {
       await post.updateOne({ $pull: { likes: userId } });
-      const likedDislikedpost = await PostModel.findById(id)
-      res.status(200).json({message:"✅ Post disliked 👎", post:likedDislikedpost});
+      const likedDislikedpost = await PostModel.findById(id);
+      res
+        .status(200)
+        .json({ message: "✅ Post disliked 👎", post: likedDislikedpost });
     } else {
       await post.updateOne({ $push: { likes: userId } });
-      const likedDislikedpost = await PostModel.findById(id)
-      res.status(200).json({message:"✅ Post liked 👍",post:likedDislikedpost});
+      const likedDislikedpost = await PostModel.findById(id);
+      res
+        .status(200)
+        .json({ message: "✅ Post liked 👍", post: likedDislikedpost });
     }
   } catch (error) {
-    res.status(500).json({message:"❌ post could not be like/disliked:", error:error.message});
+    res
+      .status(500)
+      .json({
+        message: "❌ post could not be like/disliked:",
+        error: error.message,
+      });
+  }
+};
+
+/* timeline posts */
+export const getTimelinePosts = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const currentUserPosts = await PostModel.find({ userId: userId });
+
+    const followingPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    const recentPosts = currentUserPosts
+      .concat(...followingPosts[0].followingPosts)
+      .sort((a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      })
+    res.status(200).json(recentPosts);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "❌ error in timeline posts:", error: error.message });
   }
 };
--- advance-server/src/Api/Routes/PostRoute.js
+++ advance-server/src/Api/Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createPost, deletePost, getAllPosts, getPost, likePost, updatePost } from "../Controllers/PostController.js";
+import { createPost, deletePost, getAllPosts, getPost, getTimelinePosts, likePost, updatePost } from "../Controllers/PostController.js";
 
 const router = express.Router();
 router.post("/",createPost);
@@ -8,6 +8,6 @@ router.get("/",getAllPosts)
 router.put('/:id', updatePost);
 router.delete('/:id', deletePost);
 router.put('/:id/like', likePost);
-
+router.get('/:id/timeline', getTimelinePosts);
 
 export default router;
