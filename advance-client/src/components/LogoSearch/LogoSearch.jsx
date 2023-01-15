d07fbdc4f17f3128085c49d8728ff0b024550267
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-01-14 21:04:20 -0500

    feature: create authMiddleWare.

--- advance-client/src/components/LogoSearch/LogoSearch.jsx
+++ advance-client/src/components/LogoSearch/LogoSearch.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Logo from "../../img/logo.png"
-import searchIcon from "../../img/search-icon.png"
-import "./LogoSearch.css"
+import React from "react";
+import Logo from "../../img/logo.png";
+import searchIcon from "../../img/search-icon.png";
+import {Link} from "react-router-dom";
+import "./LogoSearch.css";
 
 export default function LogoSearch() {
   return (
     <div className="LogoSearch">
-      <img className='LogoSearch-logo' src={Logo} alt="AdvanceLogo" />
+      <Link to="../../home">
+        <img className="LogoSearch-logo" src={Logo} alt="AdvanceLogo" />
+      </Link>
       <div className="Search">
-        <input className="LogoSearch-input" type="text" placeholder='#Get Inspired' />
+        <input
+          className="LogoSearch-input"
+          type="text"
+          placeholder="#Get Inspired"
+        />
         <div className="s-icon">
           <img src={searchIcon} alt="" className="search-icon" />
         </div>
       </div>
     </div>
-  )
+  );
 }
